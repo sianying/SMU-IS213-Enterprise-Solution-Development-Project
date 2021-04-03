7d37bd4c4b515874f19d7b4aba8e3db0e4060455
jingweisim <76493280+jingweisim@users.noreply.github.com>
2021-04-03 21:57:26 +0800

    I added update update to customer.py

--- customer/customer.py
+++ customer/customer.py
@@ -9,6 +9,7 @@ CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/customer'
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:password@localhost:3306/customer'
 #there is a need for authentication to the database using root (user) and pass(if there is any)
+#update update
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
