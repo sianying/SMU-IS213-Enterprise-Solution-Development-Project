072e821b054cbc995c01298d32968b2953027113
jingweisim <76493280+jingweisim@users.noreply.github.com>
2021-04-07 05:12:05 +0800

    Updated Login (hashing) & register user

--- login/login.py
+++ login/login.py
@@ -5,13 +5,17 @@ from flask_cors import CORS
 import os
 from os import environ
 
+import bcrypt
+
 app = Flask(__name__)
 CORS(app)
 
 HOST="0.0.0.0"
 PORT = 5005
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/login'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/login'
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('dbURL')
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:password@localhost:3306/driver'
 #there is a need for authentication to the database using root (user) and pass(if there is any)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -23,24 +27,25 @@ class Login(db.Model):
     username = db.Column(db.String(64), primary_key=True)
     password = db.Column(db.String(12), nullable=False)
     account_type = db.Column(db.String(8), nullable=False)
-    ID = db.Column(db.Integer, nullable=False) 
+    customer_ID = db.Column(db.Integer, nullable=True) 
+    driver_ID = db.Column(db.Integer, nullable=True) 
     # customer_ID = db.Column(db.Integer, nullable=True)
     # driver_ID = db.Column(db.Integer, nullable=True)
 
-    def __init__(self, username, password, account_type, ID):
+    def __init__(self, username, password, account_type, customer_ID, driver_ID):
         self.username = username
         self.password = password
         self.account_type = account_type
-        self.ID = ID
-        # self.customer_ID = customer_ID
-        # self.driver_ID = driver_ID
+        self.customer_ID = customer_ID
+        self.driver_ID = driver_ID
 
     def json(self):
         return {
             "username": self.username, 
             "password": self.password, 
             "account_type": self.account_type, 
-            "ID": self.ID
+            "customer_ID": self.customer_ID,
+            "driver_ID": self.driver_ID
         }
 
 #verify if a user exists
@@ -48,97 +53,124 @@ class Login(db.Model):
 def authenticate_user():
     user_data = request.get_json()
     user_recorded = Login.query.filter_by(username=user_data['username']).first()
+    account_type = user_recorded.account_type
+    #eg. customer_ID or driver_ID
+    user_ID_header = account_type + "_ID"
+    user_ID = user_recorded.user_ID_header
     if user_recorded:
         username = user_data['username']
         password = user_data['password']
         account_type = user_data['account_type']
-        if username == user_recorded.username and password == user_recorded.password:
-            if account_type=="customer":
-                print("Authenticated! Welcome User " + username)
-                return jsonify(
-                    {
-                        "code": 200,
-                        "data": {
-                            "username":user_recorded.username,
-                            "customer_ID": user_recorded.ID,
-                            "account_type": "customer"
-                        },
-                        "message": "Login is successful"
-                    }
-                )
-            else:
-                return jsonify(
-                    {
-                        "code": 200,
-                        "data": {
-                            "username":user_recorded.username,
-                            "driver_ID": user_recorded.ID,
-                            "account_type": "driver"
-                        },
-                        "message": "Login is successful"
-                    }
-                )
+
+        hashed = user_recorded['password']
+        # if username == user_recorded.username and password == user_recorded.password:
+        if username == user_recorded.username and bcrypt.checkpw(password, hashed):
+            print("Authenticated! Welcome User " + username)
+            return jsonify({
+                    "code": 200,
+                    "data": {
+                        "username":user_recorded.username,
+                        user_ID_header: user_recorded.user_ID,
+                        "account_type": account_type
+                    },
+                    "message": "Login is successful"
+                })
     print("Unable to login, please sign up with Cheetah Express ;)")
-    return jsonify(
-        {
+    return jsonify({
             "code": 404,
             "message": "User not found. Please sign up for an account with Cheetah Go ;)."
-        }
-    ), 404
+        }), 404
     
+
 #check if username is taken 
 @app.route("/check_username_exist/<string:username>", methods=['GET'])
 def check_username_exist(username):
     user_recorded = Login.query.filter_by(username=username).first()
     if (user_recorded):
-        code = 400
-        message = "The username is taken."
+        return jsonify({
+            "code": 400,
+            "username": username,
+            "message": "The username is taken :("
+        }), 400
     else:
-        code = 200
-        message = "The username is available."
-    return jsonify({
-        "code": code,
-        "username": user_recorded['username'],
-        "message": message
-        })
+        return jsonify({
+            "code": 200,
+            "username": username,
+            "message": "The username is available."
+        }), 200
 
+
+#creates user
 @app.route("/register_account/<string:username>", methods=['POST'])
 def register_user(username):
     #POST request data
     # {
     #     "password": password,
     #     "account_type": account_type,
-    #     "ID": ID
+    #     "customer_ID": customer_ID or "driver_ID": driver_ID
     # }
 
+    #verifies if the user exists, but this is covered in covered in the other function
+    # user_recorded = Login.query.filter_by(username=username).first()
+    # if user_recorded:
+    #     account_type = user_recorded.account_type
+    #     if account_type == "customer":
+    #         return jsonify({
+    #             "code": 400,
+    #             "data": {
+    #                 "username": user_recorded.username,
+    #                 "account_type": user_recorded.account_type,
+    #                 "customer_ID": user_recorded.customer_ID
+    #             },
+    #             "message": "User already exists. Please choose another username."
+    #         }), 400
+    #     else:
+    #         return jsonify({
+    #             "code": 400,
+    #             "data": {
+    #                 "username": user_recorded.username,
+    #                 "account_type": user_recorded.account_type,
+    #                 "driver_ID": user_recorded.driver_ID
+    #             },
+    #             "message": "User already exists. Please choose another username."
+    #             }), 400
+
     data = request.get_json()
+    account_type = data['account_type']
+    user_ID_header = account_type  + "_ID"
+
     account = Login(username, **data)
     try:
         db.session.add(account)
         db.session.commit()
     except:
-        return jsonify(
-            {
+        return jsonify({
                 "code": 500,
                 "data": {
                     "username": username,
-                    "account_type": data['account_type'],
-                    "ID": data['ID']
+                    "account_type": account_type,
+                    user_ID_header: data[user_ID_header]
                 },
                 "message": "An error occurred creating the account."
-            }
-        ), 500
+            }), 500
     
     return jsonify(
         {
             "code": 201,
             'data': {
                 "username": username,
-                "account_type": data['account_type'],
-                "ID": data['ID']
+                "account_type": account_type,
+                user_ID_header: data[user_ID_header]
             }
         }
     ), 201
+
+def create_hash_password(password):
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(password, salt)
+
+    return hashed
+
 
 
 if __name__ =='__main__':
