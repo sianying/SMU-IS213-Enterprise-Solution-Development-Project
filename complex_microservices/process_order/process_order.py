bd606bb1f5ad3dd6896dfaab8f033806f88631e8
zonghanlee <76741754+zonghanlee@users.noreply.github.com>
2021-04-07 07:51:13 +0800

    Update process_order.py

--- complex_microservices/process_order/process_order.py
+++ complex_microservices/process_order/process_order.py
@@ -213,7 +213,7 @@ def send_notification(order):
     receiver_name = order['data']['receiver_name']
     created = order['data']['created']
 
-    customer_message = "Delivery Completed! +\n\nDelivery Order ID: " + str(delivery_ID) + " to " + str(receiver_name) +" has been successfully created on " + str(created) + "\nPlease proceed to 'View My Deliveries' to catch a glimpse!"
+    customer_message = "Delivery Created! +\n\nDelivery Order ID: " + str(delivery_ID) + " to " + str(receiver_name) +" has been successfully created on " + str(created) + "\nPlease proceed to 'View My Deliveries' to catch a glimpse!"
     driver_message = "You have a new delivery order! \n\nDelivery Order ID: " + str(delivery_ID) + " has been successfully created on " + str(created) + "\nPlease proceed to 'View My Deliveries' or 'View my Schedule' to catch a glimpse!"
 
     messages = json.dumps({
