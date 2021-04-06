270bccd0d47e473a80da10561cfec1eb344504ae
zonghanlee <76741754+zonghanlee@users.noreply.github.com>
2021-04-07 03:56:33 +0800

    Update error.py

--- Notification/error.py
+++ Notification/error.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # The above shebang (#!) operator tells Unix-like environments
 # to run this file as a python3 script
-from flask import request
+#from flask import request
 import json
 import os
 
